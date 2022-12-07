c8d625328898d013eeb431334566cc67780b62b0
Jahongir Turgunboyev <j.turgunboyev@technocorp.uz>
2022-12-07 11:24:13 +0500

    7day

--- 7day_1ex.py
+++ 7day_1ex.py
@@ -28,15 +28,10 @@ with open("input.txt") as f:
             except:
                 pass
 
-max_used = 70_000_000 - 30_000_000
-total = SZ['/']
-need_to_free = total - max_used
 ans = 0
-best = 1e9
-for k, v in SZ.items():
-    if v >= need_to_free:
-        best = min(best, v)
 
+for k, v in SZ.items():
     if v <= 100000:
         ans += v
-print(best)
+print(ans)
+
